8adb3c91399e14e5152d75507a0be92476e2001e
Dominic Kempf <dkempf2@web.de>
2023-07-18 11:24:25 +0200

    feat: added rng color api

--- web-apps/color_mixer_API/script.js
+++ web-apps/color_mixer_API/script.js
@@ -3,6 +3,7 @@ const greenSlider = document.getElementById("greenSlider");
 const blueSlider = document.getElementById("blueSlider");
 const colorMixer = document.getElementById("color-mixer");
 const colorValue = document.getElementById("color-value");
+const randomColorButton = document.getElementById("randomColorButton");
 
 function updateColor() {
   const red = redSlider.value;
@@ -14,9 +15,27 @@ function updateColor() {
   colorValue.textContent = color;
 }
 
+function getRandomColor() {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => response.json())
+    .then((data) => {
+      const {
+        rgb: { r, g, b },
+      } = data;
+      redSlider.value = r;
+      greenSlider.value = g;
+      blueSlider.value = b;
+      updateColor();
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
 redSlider.addEventListener("input", updateColor);
 greenSlider.addEventListener("input", updateColor);
 blueSlider.addEventListener("input", updateColor);
+randomColorButton.addEventListener("click", getRandomColor);
 
 // Initial color values (default color)
 redSlider.value = 255;
